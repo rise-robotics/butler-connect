08686fec999a56a1e832c03b618c7f1f2289758d
Rav Panchalingam <rav@risebt.com.au>
2025-08-31 15:15:26 +0800

    style: Format PM2 ecosystem config
    - Improve indentation and formatting in ecosystem.config.js
    - No functional changes, just code style improvements

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,27 +1,27 @@
 module.exports = {
-  apps: [{
-    name: 'butler-connect',
-    script: 'src/main.py',
-    interpreter: './venv/bin/python',
-    cwd: '/home/rav/projects/butler-connect',
-    instances: 1,
-    autorestart: true,
-    watch: ['src'],
-    watch_delay: 1000,
-    watch_options: {
-      followSymlinks: false
-    },
-    ignore_watch: [
-      "node_modules",
-      "logs",
-      "__pycache__",
-      "*.pyc",
-      ".git",
-      ".vscode",
-      "venv",
-      "*.log"
-    ],
-    max_memory_restart: '1G',
+    apps: [{
+        name: 'butler-connect',
+        script: 'src/main.py',
+        interpreter: './venv/bin/python',
+        cwd: '/home/rav/projects/butler-connect',
+        instances: 1,
+        autorestart: true,
+        watch: ['src'],
+        watch_delay: 1000,
+        watch_options: {
+            followSymlinks: false
+        },
+        ignore_watch: [
+            "node_modules",
+            "logs",
+            "__pycache__",
+            "*.pyc",
+            ".git",
+            ".vscode",
+            "venv",
+            "*.log"
+        ],
+        max_memory_restart: '1G',
         env: {
             NODE_ENV: 'production',
             PYTHONPATH: '/home/rav/projects/butler-connect/src'
